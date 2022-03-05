3b84f8f5c5f1618646f6f827fead33e0fc1b7cd9
lockcept <lockcept@gmail.com>
2022-03-05 20:53:30 +0900

    feat: hero

--- packages/app/src/Hero.tsx
+++ packages/app/src/Hero.tsx
@@ -3,13 +3,23 @@ import React from "react";
 export default function Hero() {
   return (
     <section id="hero">
-      <div className="flex justify-center items-center py-16 min-h-screen">
-        <div className="w-128 h-32 flex flex-col justify-center items-center">
-          <div className="text-black text-6xl font-bold text-center flex-1">
+      <div className="flex justify-center items-center py-16 min-h-screen px-8">
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-black text-6xl font-bold text-center flex-1 mb-8">
             <span>Welcome to </span>
             <span className="text-green-500">LOCKCEPT</span>
             <span>ed</span>
           </div>
+
+          <button
+            type="button"
+            className="mt-8 inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-cyan-400 hover:bg-cyan-500"
+            onClick={() => {
+              window.location.href = "#about";
+            }}
+          >
+            더 보기
+          </button>
         </div>
       </div>
     </section>
