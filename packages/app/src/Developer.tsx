ff0c8393e9db28794e744ce31739c675c8d63614
lockcept <lockcept@gmail.com>
2022-03-06 18:42:17 +0900

    feat: about

--- packages/app/src/Developer.tsx
+++ packages/app/src/Developer.tsx
@@ -5,7 +5,7 @@ export default function Developer() {
     <section id="developer">
       <div className="flex justify-center items-center py-16 min-h-screen">
         <div className="flex flex-col justify-center items-center">
-          <div className="text-black text-6xl font-bold text-center flex-1">
+          <div className="font-bold flex-1">
             <span>개발자 </span>
             <span className="text-green-500">록셉</span>
           </div>
