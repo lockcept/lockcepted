79fefdcdf21b5df0f5aa68a42e1bb9bc867115ff
lockcept <lockcept@gmail.com>
2022-03-07 04:49:35 +0900

    feat: Divider

--- packages/app/src/About.tsx
+++ packages/app/src/About.tsx
@@ -3,16 +3,16 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="flex justify-center items-center py-16 min-h-screen bg-green-200 px-8">
+      <div className="flex justify-center items-center py-16 min-h-screen px-8">
         <div className="flex flex-col justify-center items-center w-full">
           <div className="flex-1 font-bold mb-8">
             <span>About </span>
             <span className="text-green-500">록셉</span>
           </div>
           <div className="flex my-8 w-full">
-            <div className="flex-1 p-4 flex justify-center">
+            <div className="flex-1 p-4 flex justify-center items-center">
               <img
-                className="ring-4 ring-green-100 rounded-full"
+                className="ring-2 ring-green-100 ring-opacity-10 rounded-full"
                 src="/lockcept.png"
                 alt="lockcept"
               />
@@ -29,6 +29,9 @@ export default function About() {
                 <br />
                 <br />
                 저는 나만의 세계를 만들어 나가는 것을 좋아합니다.
+                <br />
+                <br />
+                저는 항상 근거를 가지고 문제를 해결하려고 노력합니다.
                 <br />
                 <br />
                 저는 서울대학교 산업공학과 주전공, 컴퓨터공학부 부전공을
